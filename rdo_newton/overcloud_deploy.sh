3308f373ec455f22b897e08072561ce1352c8245
Zenghui Shi <zshi@redhat.com>
2017-12-14 20:15:10 +0800

    add sriov configuration
    1) add first-boot.yaml
    2) add post-install.yaml
    3) modify network-environment.yaml file
    4) modify compute and controller nic-configs
    5) modify overcloud_deploy.sh

--- rdo_newton/overcloud_deploy.sh
+++ rdo_newton/overcloud_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-openstack overcloud deploy --templates --control-scale 3 --compute-scale 2 --ceph-storage-scale 0 --control-flavor controller --compute-flavor compute -e /usr/share/openstack-tripleo-heat-templates/environments/puppet-pacemaker.yaml -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml -e /usr/share/openstack-tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml  -e /home/stack/rdo_newton/network-environment.yaml --ntp-server pool.ntp.org --log-file overcloud_install.log
+openstack overcloud deploy --templates --control-scale 3 --compute-scale 2 --ceph-storage-scale 0 --control-flavor controller --compute-flavor compute -e /usr/share/openstack-tripleo-heat-templates/environments/puppet-pacemaker.yaml -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml -e /usr/share/openstack-tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml  -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml -e /home/stack/rdo_newton/network-environment.yaml --ntp-server pool.ntp.org --log-file overcloud_install.log
