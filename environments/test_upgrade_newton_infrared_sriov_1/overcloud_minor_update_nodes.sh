4325af87f82c503b2d3248f593c3041fb37fcad4
Yolanda Robla <yroblamo@redhat.com>
2018-01-10 09:44:16 +0100

    Add new script to perform the minor update

--- environments/test_upgrade_newton_infrared_sriov_1/overcloud_minor_update_nodes.sh
+++ environments/test_upgrade_newton_infrared_sriov_1/overcloud_minor_update_nodes.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 source /home/stack/stackrc
-function update_node_overcloud {
-    local NODE_HOST=$1
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no heat-admin@$1 'sudo yum -y install python-pip'
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no heat-admin@$1 'cd /opt; sudo git clone https://github.com/openstack/tripleo-repos || true'
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no heat-admin@$1 'cd /opt/tripleo-repos; sudo pip install ./'
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no heat-admin@$1 'sudo tripleo-repos -b master current; sudo yum update -y kernel openvswitch;'
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no heat-admin@$1 'sudo tripleo-repos -b newton current'
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no heat-admin@$1 'sudo rm -rf /etc/yum.repos.d/rhos-*'
-}
-source /home/stack/stackrc
-IPS=$(nova list | grep overcloud | awk '{ split($12, v, "="); print v[2]}')
-readarray -t IPS <<<"$IPS"
-for IP in "${IPS[@]}"; do
-    update_node_overcloud $IP
-done
+
+cd /opt
+
+if [ -d  /opt/infrared_upgrade_config ]; then
+    cd infrared_upgrade_config
+    git pull
+else
+    sudo git clone https://github.com/redhat-nfvpe/infrared_upgrade_config
+    cd infrared_upgrade_config
+fi
+sudo chown -R stack:stack /opt/infrared_upgrade_config
+
+cd /opt/infrared_upgrade_config
+cp ovs_upgrade/playbooks/ovs_upgrade.yml ./
+
+bash ovs_upgrade/inventory/generate_inventory.sh
+ansible-playbook -i /home/stack/hosts ovs_upgrade.yml
