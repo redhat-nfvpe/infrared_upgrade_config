f74963a58d0bebc881dee573a9a1c894645224f0
Yolanda Robla <yroblamo@redhat.com>
2018-01-10 20:55:38 +0100

    Fixed in script to re-test ping between vms

--- tests/sriov_pingtest_after.sh
+++ tests/sriov_pingtest_after.sh
@@ -3,10 +3,23 @@
 LOGIN_USER=$1
 
 source /home/stack/overcloudrc
+FIP_1=$(openstack server show test-sriov_vf_1 -f value -c addresses | grep -oE '10\.9\.88\.[0-9]*')
+FIP_2=$(openstack server show test-sriov_vf_2 -f value -c addresses | grep -oE '10\.9\.88\.[0-9]*')
 
+# start the vms as they were stopped after reboot
+openstack server start test-sriov_vf_1
+openstack server start test-sriov_vf_2
+sleep 5
+
+# connect and recreate ip config
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${LOGIN_USER}@${FIP_1} 'sudo ip addr add 10.0.10.2/24 dev eth1'
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${LOGIN_USER}@${FIP_1} 'sudo ip link set eth1 up'
+
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${LOGIN_USER}@${FIP_2} 'sudo ip addr add 10.0.10.3/24 dev eth1'
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${LOGIN_USER}@${FIP_2} 'sudo ip link set eth1 up'
 
 # now test ping between vms
-OUTPUT=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${LOGIN_USER}@${LAST_FIP_1} 'ping -q -w 1 10.0.10.3; echo "result$?"')
+OUTPUT=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${LOGIN_USER}@${FIP_1} 'ping -q -w 1 10.0.10.3; echo "result$?"')
 if [[ "$OUTPUT" == *"result1"* ]]; then
     echo "ping failed"
     exit 1
@@ -14,11 +27,10 @@ else
     echo "ping successful"
 fi
 
-OUTPUT=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${LOGIN_USER}@${LAST_FIP_2} 'ping -q -w 1 10.0.10.2; echo "result$?"')
+OUTPUT=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${LOGIN_USER}@${FIP_2} 'ping -q -w 1 10.0.10.2; echo "result$?"')
 if [[ "$OUTPUT" == *"result1"* ]]; then
     echo "ping failed"
     exit 1
 else
     echo "ping successful"
 fi
-
