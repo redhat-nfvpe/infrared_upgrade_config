b74278ab429170a65094a6f6c8940d35e343e2d8
Yolanda Robla <yroblamo@redhat.com>
2018-02-08 16:15:30 +0100

    Fix typo

--- tests/pingtest_continuous_recover.sh
+++ tests/pingtest_continuous_recover.sh
@@ -4,7 +4,7 @@ LOGIN_USER=$1
 VNF_IP=$(openstack server list -f value -c Name -c Networks | grep vf_1 |  sed 's/.*, \(.*\);.*/\1/')
 
 # first kill the ping process if still runs
-ssh $LOGIN_USER@$VNF_IP "PROCESS=$(pgrep -f 'ping -D -w 4000 10.0.10.3'); kill-2 $PROCESS"
+ssh $LOGIN_USER@$VNF_IP "PROCESS=$(pgrep -f 'ping -D -w 4000 10.0.10.3'); kill -2 $PROCESS"
 
 # then output file content
 ssh $LOGIN_USER@$VNF_IP "cat /tmp/pingtest_output"
