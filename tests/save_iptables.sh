9ac4fc242061ea870048ae05f1b348e2603de917
Yolanda Robla <yroblamo@redhat.com>
2018-02-14 14:31:58 +0100

    Fix naming on iptables

--- tests/save_iptables.sh
+++ tests/save_iptables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source /home/stack/stackrc
 
-COMPUTE1_IP=$(openstack server list -f value -c Name -c Networks | grep novacompute-0 |  cut -d ' ' -f2 | cut -d '=' -f2 )
-COMPUTE2_IP=$(openstack server list -f value -c Name -c Networks | grep novacompute-1 |  cut -d ' ' -f2 | cut -d '=' -f2 )
+COMPUTE1_IP=$(openstack server list -f value -c Name -c Networks | grep compute-0 |  cut -d ' ' -f2 | cut -d '=' -f2 )
+COMPUTE2_IP=$(openstack server list -f value -c Name -c Networks | grep compute-1 |  cut -d ' ' -f2 | cut -d '=' -f2 )
 
 # ssh into the vms and save iptables
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null heat-admin@${COMPUTE1_IP} 'sudo bash -c "iptables-save > /etc/sysconfig/iptables"'
