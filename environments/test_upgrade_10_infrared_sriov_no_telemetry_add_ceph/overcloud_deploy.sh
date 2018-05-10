8aa4c2fa6045b92e3099ad87cfec7644f82eec34
Yolanda Robla <yroblamo@redhat.com>
2018-05-10 13:22:22 +0200

    Add updated environment for ceph deploy
    Include the new firstboot-scripts folder, that includes
    commands to wipe ceph disks. Also add ffu_repo.yaml, that
    will setup rhos-release repos for ffu.

--- environments/test_upgrade_10_infrared_sriov_no_telemetry_add_ceph/overcloud_deploy.sh
+++ environments/test_upgrade_10_infrared_sriov_no_telemetry_add_ceph/overcloud_deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-openstack overcloud deploy --templates --control-scale 3 --compute-scale 1 --ceph-storage-scale 1 \
+openstack overcloud deploy --templates \
+--control-scale 3 \
+--compute-scale 1 \
+--ceph-storage-scale 1 \
 -e /usr/share/openstack-tripleo-heat-templates/environments/puppet-pacemaker.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml \
 -e /home/stack/test_upgrade_10_infrared_sriov_no_telemetry_add_ceph/network-environment.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/storage-environment.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-sriov.yaml \
---ntp-server clock.redhat.com --log-file overcloud_install.log
+--ntp-server clock.redhat.com \
+--log-file overcloud_install.log
 
